565ab1ed5c05dcaad03d64f6d4eb63d1e7c55a94
Davi-Ga <davigaldinoky@gmail.com>
2022-04-22 16:17:30 -0300

    title e version do projeto adicionados

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
     
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -14,7 +15,6 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def read_root():
     return {"localhost/1"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
