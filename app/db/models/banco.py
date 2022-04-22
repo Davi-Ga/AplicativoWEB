2d47b3477d9bf105ae02a0461228ec543502dd67
Davi-Ga <davigaldinoky@gmail.com>
2022-04-22 03:07:31 -0300

    banco e agencia definidos

---
+++ app/db/models/banco.py
@@ -0,0 +1,6 @@
+from db.base_class import Base
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
