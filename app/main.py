6718477c58abb6cdecd0780669354f706c19e552
Davi-Ga <davigaldinoky@gmail.com>
2022-04-22 01:51:27 -0300

    main organizado

--- app/main.py
+++ app/main.py
@@ -6,20 +6,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
     
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"localhost/1"}
 
 
 @app.get("/items/{item_id}")
@@ -29,7 +23,3 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.get("/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
