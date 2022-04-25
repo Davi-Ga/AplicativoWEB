3bdf61504d44fb71d243a126144a4345e6b6d16d
Davi-Ga <davigaldinoky@gmail.com>
2022-04-25 19:17:38 -0300

    urlpatterns definidas

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-admin.autodiscover()
+from django.urls import path,include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('banco/', include('banco.urls')),
     path('admin/', admin.site.urls),
+]
+
+#Add URL maps to redirect the base URL to our application
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
 ]
