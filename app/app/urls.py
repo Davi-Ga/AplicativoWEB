51f6125d90863b616eea300add1d3d7b2465413b
Davi-Ga <davigaldinoky@gmail.com>
2022-04-26 00:38:31 -0300

    redirect view configurado e static carregado

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('banco/', include('banco.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Add URL maps to redirect the base URL to our application
 
 urlpatterns += [
-    path('', RedirectView.as_view(url='/catalog/')),
+    path('', RedirectView.as_view(url='/banco/')),
 ]
