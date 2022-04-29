48d5eb56913da469f0d03d7660c385138441c3cb
Davi-Ga <davigaldinoky@gmail.com>
2022-04-29 14:45:27 -0300

    models definido

--- src/models.py
+++ src/models.py
@@ -1,18 +1,33 @@
 from config import db
+from flask_sqlalchemy import SQLAlchemy
+
+TIPO_PHONE=[
+    ('0','Celular'),
+    ('1','Fixo'),
+]
 
 class Agencia(db.Model):
     __tablename__ = 'Agencia'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True,null=False,autoincrement=True)
+    id_banco = db.Column(db.String(),null=False)
+    endereco = db.Column(db.String())
+    tipo = db.Column(db.Integer,choice=TIPO_PHONE)
+    fone = db.Column(db.String())
+    tipo1 = db.Column(db.Integer,choice=TIPO_PHONE)
+    fone1 = db.Column(db.String())
+    agencia = db.Column(db.String())
+    nome_agencia = db.Column(db.String())
+    
 
-    def __init__(self, name, author, published):
-        self.name = name
-        self.author = author
-        self.published = published
+    def __init__(self, id_banco,endereco,tipo,fone,tipo1,fone1,agencia,nome_agencia):
+        self.id_banco = id_banco
+        self.endereco = endereco
+        self.tipo = tipo
+        self.fone=fone
+        self.tipo1=tipo1
+        self.fone1=fone1
+        self.agencia=agencia
+        self.nome_agencia=nome_agencia
 
-    def __repr__(self):
-        return f"<Car {self.name}>"
     
