cfe8129515a17a0e517b89f56355f50abad455a8
Davi-Ga <davigaldinoky@gmail.com>
2022-04-29 22:39:42 -0300

    Models definidos e ajustados

--- src/models.py
+++ src/models.py
@@ -7,27 +7,36 @@ TIPO_PHONE=[
 ]
 
 class Agencia(db.Model):
-    __tablename__ = 'Agencia'
+    __tablename__='Agencia'
 
-    id = db.Column(db.Integer, primary_key=True,null=False,autoincrement=True)
-    id_banco = db.Column(db.String(),null=False)
-    endereco = db.Column(db.String())
-    tipo = db.Column(db.Integer,choice=TIPO_PHONE)
-    fone = db.Column(db.String())
-    tipo1 = db.Column(db.Integer,choice=TIPO_PHONE)
-    fone1 = db.Column(db.String())
-    agencia = db.Column(db.String())
-    nome_agencia = db.Column(db.String())
+    id=db.Column(db.Integer, primary_key=True,null=False,autoincrement=True)
+    id_banco=db.Column(db.String(200),null=False)
+    endereco=db.Column(db.String(200))
+    tipo=db.Column(db.Integer,choice=TIPO_PHONE)
+    fone=db.Column(db.String(200))
+    tipo1=db.Column(db.Integer,choice=TIPO_PHONE)
+    fone1=db.Column(db.String(200))
+    agencia=db.Column(db.String(200))
+    nome_agencia=db.Column(db.String(200))
     
-
     def __init__(self, id_banco,endereco,tipo,fone,tipo1,fone1,agencia,nome_agencia):
-        self.id_banco = id_banco
-        self.endereco = endereco
-        self.tipo = tipo
+        self.id_banco=id_banco
+        self.endereco=endereco
+        self.tipo=tipo
         self.fone=fone
         self.tipo1=tipo1
         self.fone1=fone1
         self.agencia=agencia
         self.nome_agencia=nome_agencia
-
+        
+        
+class Banco(db.Model):
+    __tablename__='Banco'
     
+    id=db.Column(db.Integer,primary_key=True,null=False,autoincrement=True)
+    nome=db.Coumn(db.String(200))
+    numero=db.Column(db.String(200))
+
+    def __init__(self,nome,numero):
+        self.nome=nome
+        self.numero=numero
