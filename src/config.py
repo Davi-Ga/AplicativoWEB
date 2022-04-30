dd4248ab6fe2810e42a22d4236e649c8bde9b0da
Davi-Ga <davigaldinoky@gmail.com>
2022-04-29 22:39:27 -0300

    Imports organizados

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 # Previous imports remain...
-import os
-from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import app
 
 
+from dotenv import load_dotenv
+import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+import app
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URL'))
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
