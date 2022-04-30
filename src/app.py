5d267f179d3b2694e6a8a53534681f81af686eea
Davi-Ga <davigaldinoky@gmail.com>
2022-04-30 20:03:59 -0300

    app.run para realizar debugs

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, jsonify, make_response,redirect
+from dotenv import load_dotenv
+
 
 app = Flask(__name__,static_url_path='',static_folder='static', template_folder='templates')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
 @app.route("/",methods=['GET'])
 def index_get():
     return render_template("index.html")
+
+app.run(debug=True)
