f87b2d41f46ecb9b461e96a7f5cc19f41091576d
Davi-Ga <davigaldinoky@gmail.com>
2022-05-02 17:15:04 -0300

    app ajustado para o debug

--- src/app.py
+++ src/app.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 app = Flask(__name__,static_url_path='',static_folder='static', template_folder='templates')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
     
 @app.route("/",methods=['GET'])
 def index_get():
     return render_template("index.html")
 
-app.run(debug=True)
+
