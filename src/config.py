d52c4ba89b9fe3756b2c590ed05932fe5df22b1e
Davi-Ga <davigaldinoky@gmail.com>
2022-05-02 20:00:59 -0300

    import app adicionado

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-import app
+from app import app
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URL'))
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
