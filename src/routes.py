1c22c390c95428aafe7c41c20c49c3b8024ac1cd
Davi-Ga <davigaldinoky@gmail.com>
2022-05-02 20:00:47 -0300

    rotas adicionado

---
+++ src/routes.py
@@ -0,0 +1,8 @@
+# from flask import Flask, render_template, jsonify, make_response,redirect
+# from dotenv import load_dotenv
+# from app import app
+
+    
+# @app.route("/",methods=['GET'])
+# def index_get():
+#     return render_template("index.html")
