3fad7744cef8cea32804b688f0f77afd4e91ec79
Davi-Ga <davigaldinoky@gmail.com>
2022-05-04 13:58:20 -0300

    estruturação refeita

--- src/routes.py
+++
@@ -1,8 +0,0 @@
-# from flask import Flask, render_template, jsonify, make_response,redirect
-# from dotenv import load_dotenv
-# from app import app
-
-    
-# @app.route("/",methods=['GET'])
-# def index_get():
-#     return render_template("index.html")
