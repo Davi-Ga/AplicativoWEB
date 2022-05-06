b91ffd4017d342a2174cee21b57ee3a84ec030e7
Davi-Ga <davigaldinoky@gmail.com>
2022-05-06 20:30:34 -0300

    home html ajustado

--- app/banco/views.py
+++ app/banco/views.py
@@ -1,7 +1,10 @@
+from django.views.generic import ListView
 from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from banco.models import Agencia
 
 class PaginaInicial(TemplateView):
-    template_name = 'banco/index.html'
+    template_name = 'banco/home.html'
+    
