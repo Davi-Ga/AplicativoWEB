46e0ecf2ba7c237ad9fcb3154cbf6f0e2e2cc433
Davi-Ga <davigaldinoky@gmail.com>
2022-05-07 01:02:27 -0300

    banco form adicionado

--- app/banco/forms.py
+++ app/banco/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 from banco.models import Agencia
+from banco.models import Banco
+
+class BancoForm(forms.ModelForm):
+    class Meta:
+        model = Banco
+        fields = '__all__'
 
 class AgenciaForm(forms.ModelForm):
     class Meta:
