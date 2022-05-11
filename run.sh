ed475f6d5dd8238b2caca0a224a561a78fc61d85
Davi-Ga <davigaldinoky@gmail.com>
2022-05-11 15:17:34 -0300

    volume ajustado

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 VOLUMES="${docker volume ls | postgres-flask | awk '{print $2}'}"
 
 if [ -n "$VOLUMES"]; then
-    docker volume create postgres-flask
+    docker volume create postgres-data
 fi
 
 docker-compose build
