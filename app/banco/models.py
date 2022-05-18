379ed517edcde19f5db8ceb1736d1db5175709de
Davi-Ga <davigaldinoky@gmail.com>
2022-05-18 14:07:05 -0300

    models ajsutado para retornar numero e nome

--- app/banco/models.py
+++ app/banco/models.py
@@ -8,34 +8,33 @@ TIPO_PHONE =[
 
 class Banco(models.Model):
     
-
     nome = models.CharField(max_length=250,null=False)
-    numero = models.CharField(max_length=250)
+    numero = models.CharField(max_length=250,null=False)
     
     class Meta:
         db_table = "Banco" 
     
     def __str__(self):
-        return self.nome
-
+        
+        return self.numero+" - "+self.nome 
+    
 
 class Agencia(models.Model):
     
-    
     id_banco = models.ForeignKey('Banco',on_delete=models.CASCADE)
-    endereco = models.CharField(max_length=250)
-    fone = models.CharField(max_length=11,unique=True)
-    tipo = models.IntegerField(choices=TIPO_PHONE)
-    fone1 = models.CharField(max_length=11,blank=True)
+    endereco = models.CharField(max_length=250,null=False)
+    fone = models.DecimalField(max_digits=11,decimal_places=0,unique=True,null=False)
+    tipo = models.IntegerField(choices=TIPO_PHONE,null=False)
+    fone1 = models.DecimalField(max_digits=11,decimal_places=0,blank=True)
     tipo1 = models.IntegerField(choices=TIPO_PHONE,blank=True)
-    agencia = models.CharField(max_length=250)
-    nome_agencia = models.CharField(max_length=250)
+    agencia = models.CharField(max_length=250,null=False)
+    nome_agencia = models.CharField(max_length=250,null=False)
     
     class Meta:
         db_table = "Agencia"
     
     def __str__(self):
-        return self.nome
+        return self.nome_agencia
     
 
     
