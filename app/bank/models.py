57f3d21da32304f6fc4d6c3614b2943e135007ee
Davi-Ga <davigaldinoky@gmail.com>
2022-05-24 14:22:52 -0300

    models formatado para o ingles

---
+++ app/bank/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+TYPE_PHONE =[
+    ('1','Landline phone'),
+    ('0','Mobile phone'),
+]
+
+class Bank(models.Model):
+    
+    name = models.CharField(max_length=250,null=False,unique=True)
+    number = models.CharField(max_length=250,null=False,unique=True)
+    
+    class Meta:
+        db_table = "Bank" 
+    
+    def __str__(self):
+        
+        return self.number+" - "+self.name 
+    
+
+class Agency(models.Model):
+    
+    bank_id = models.ForeignKey('Bank',on_delete=models.CASCADE)
+    address = models.CharField(max_length=250,null=False)
+    phone = models.DecimalField(max_digits=11,decimal_places=0,unique=True,null=False)
+    phonetype = models.IntegerField(choices=TYPE_PHONE,null=False)
+    phone1 = models.DecimalField(max_digits=11,decimal_places=0,blank=True)
+    phonetype1 = models.IntegerField(choices=TYPE_PHONE,blank=True)
+    agency = models.CharField(max_length=250,null=False)
+    agency_name = models.CharField(max_length=250,null=False)
+    
+    class Meta:
+        db_table = "Agency"
+    
+    def __str__(self):
+        return self.agency_name
+    
+
+    
