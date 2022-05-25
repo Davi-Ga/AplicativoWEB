4c6182c569328b7abf72c7b2f81a6df70cfed4e4
Davi-Ga <davigaldinoky@gmail.com>
2022-05-25 14:50:55 -0300

    crispy add

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bank.apps.BankConfig',
+    'crispy_forms',
+    "crispy_bootstrap5",
 
 ]
 
@@ -141,3 +143,7 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
