3a9e2da477a778f805acc253b2ab7ea9c77868d5
Davi-Ga <davigaldinoky@gmail.com>
2022-05-25 16:07:30 -0300

    forms banco

---
+++ app/banco/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from banco.models import Agencia
+from banco.models import Banco
+
+class BancoForm(forms.ModelForm):
+    
+    class Meta:
+        model = Banco
+        fields = '__all__'
+
+class AgenciaForm(forms.ModelForm):
+    class Meta:
+        model = Agencia
+        fields = '__all__'
