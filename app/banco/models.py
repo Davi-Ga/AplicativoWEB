62c3361485ddbc510f405794d2232cecce81efbe
Davi-Ga <davigaldinoky@gmail.com>
2022-05-25 16:07:42 -0300

    models ajustado para pt-br

---
+++ app/banco/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+TYPE_PHONE =[
+    ('1','Fixo'),
+    ('0','Celular'),
+]
+
+class Banco(models.Model):
+    
+    nome = models.CharField(max_length=250,null=False,unique=True)
+    numero = models.CharField(max_length=250,null=False,unique=True)
+    
+    class Meta:
+        db_table = "Banco" 
+    
+    def __str__(self):
+        
+        return self.numero+" - "+self.nome 
+    
+
+class Agencia(models.Model):
+    
+    id_banco = models.ForeignKey('Banco',on_delete=models.CASCADE)
+    endereco = models.CharField(max_length=250,null=False)
+    fone = models.BigIntegerField(unique=True,blank=True)
+    tipo = models.IntegerField(choices=TYPE_PHONE,blank=True)
+    fone1 = models.BigIntegerField(blank=True)
+    tipo1 = models.IntegerField(choices=TYPE_PHONE,blank=True)
+    agencia = models.CharField(max_length=250,null=False)
+    nome_agencia = models.CharField(max_length=250,null=False)
+    
+    class Meta:
+        db_table = "Agencia"
+    
+    def __str__(self):
+        return self.nome_agencia
+    
+
+    
