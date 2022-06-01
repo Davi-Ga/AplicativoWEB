e2238dee2d43eb75fcb1666443bf568799bf92ca
Davi-Ga <davigaldinoky@gmail.com>
2022-06-01 10:25:51 -0300

    forms ajustado

--- app/banco/forms.py
+++ app/banco/forms.py
@@ -1,14 +1,13 @@
 from django import forms
-
+from django.forms import ValidationError
 from banco.models import Agencia
 from banco.models import Banco
 
 class BancoForm(forms.ModelForm):
-    
     class Meta:
         model = Banco
         fields = '__all__'
-
+        
 class AgenciaForm(forms.ModelForm):
     class Meta:
         model = Agencia
