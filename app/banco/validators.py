4cf80a0de30a1b23ca484e51b8344c93bceb6276
Davi-Ga <davigaldinoky@gmail.com>
2022-06-01 10:57:43 -0300

    validators ajustado

--- app/banco/validators.py
+++ app/banco/validators.py
@@ -2,9 +2,8 @@ from django.core.exceptions import ValidationError
 from django import forms
 
 def nome_validator(value):
-        
-        if not len(value) > 7:
-            raise forms.ValidationError('Por favor insira um nome válido')
-        else:
-            return value
+    if not len(value) > 7:
+        raise forms.ValidationError('Insira um nome válido')
+    else:
+        return value
         
