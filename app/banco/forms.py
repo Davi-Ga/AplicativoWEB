288945b2f39a822bb03454d934e4f84dc85cbda0
Davi-Ga <davigaldinoky@gmail.com>
2022-06-06 15:45:59 -0300

    validação de número existente

--- app/banco/forms.py
+++ app/banco/forms.py
@@ -1,7 +1,9 @@
+from pydoc import getpager
 from django import forms
 from django.forms import ValidationError
 from banco.models import Agencia
 from banco.models import Banco
+
 
 class BancoForm(forms.ModelForm):
     class Meta:
@@ -12,3 +14,13 @@ class AgenciaForm(forms.ModelForm):
     class Meta:
         model = Agencia
         fields = '__all__'
+        
+    def algum_numero(self):
+        telefone=self.cleaned_data.get("fone")
+        telefone1=self.cleaned_data.get("fone1")
+        
+        if telefone and telefone1 == "":
+            raise forms.ValidationError("Insira algum número de telefone por favor")
+        return telefone and telefone1
+
+        
