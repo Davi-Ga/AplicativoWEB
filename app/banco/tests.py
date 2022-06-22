1f303722c8ecf489b702541cb5c47b1e92b239c1
Davi-Ga <davigaldinoky@gmail.com>
2022-06-22 11:18:16 -0300

    testes adicionados

--- app/banco/tests.py
+++ app/banco/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+import unittest
+from .models import Agencia
 
-# Create your tests here.
+class TestesApp(unittest.TestCase):
+    
+    def teste_aceitacao_agencia(self):
+        Agencia.objects.create(endereco='Av.Madre Maria Dos Anjos',fone=62993794033,tipo='1',fone1=62993794033,tipo1='1',agencia='23a',nome_agencia='Agencia BrasilQ')
